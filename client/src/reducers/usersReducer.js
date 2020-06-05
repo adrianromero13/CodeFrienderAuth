bddc10b5e55d7cc480115a939fa5a4134cdfc565
Mark Younan <markyounan@gmail.com>
2020-06-05 13:40:43 -0700

    made solid attempt at getting match functionality up and running. Still need to grab currentUser somehow.

--- client/src/reducers/usersReducer.js
+++ client/src/reducers/usersReducer.js
@@ -1,11 +1,15 @@
 import {
     GET_USERS,
     GET_USERS_ERROR,
+    GET_CURRENT_USER,
+    GET_CURRENT_USER_ERROR,
 } from '../actions/types';
 
 
 
 const INITIAL_STATE = {
+    currentUser: [],
+    getCurrentUserError: '',
     allUsers: [],
     getUsersError: '',
 };
@@ -13,6 +17,10 @@ const INITIAL_STATE = {
 
 export default function (state = INITIAL_STATE, action) {
     switch (action.type) {
+        case GET_CURRENT_USER:
+            return { ...state, currentUser: action.payload, };
+        case GET_CURRENT_USER_ERROR:
+            return { ...state, getCurrentUserError: action.payload };
         case GET_USERS:
             return { ...state, allUsers: action.payload, getUsersError: '' };
         case GET_USERS_ERROR:
